3a3565e288bcdbca17a99716671da23e340d349b
nparodi1 <nparodi@gmail.com>
2026-01-06 23:05:35 +0100

    Refactor code structure for improved readability and maintainability

--- example/.next/server/webpack-runtime.js
+++ example/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("d431a0d729ff20a1")
+/******/ 		__webpack_require__.h = () => ("d20d47373986aac1")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
